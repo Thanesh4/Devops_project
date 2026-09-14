f8b77bb9b513a396df7efd7765825313a9f332a1
Thanesh4 <125334925+Thanesh4@users.noreply.github.com>
2026-09-14 11:44:58 +0530

    fix(terraform): correct LabRole ARN format from // to ::

--- setup/terraform/main.tf
+++ setup/terraform/main.tf
@@ -4,7 +4,7 @@
 data "aws_caller_identity" "current" {}
 
 locals {
-  lab_role_arn = "arn:aws:iam://${data.aws_caller_identity.current.account_id}:role/LabRole"
+  lab_role_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/LabRole"
 }
 
 # Create a VPC
